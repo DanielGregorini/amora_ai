6e8b6d0fae2ec3a69dd2033ffcda0fd078b57f61
Daniel Mahl Gregorini <danielmagregorini123@gmail.com>
2026-02-19 18:37:04 +0000

    feat: matrix de confusao e novo datasetr

--- AI/preprocess_images.py
+++ AI/preprocess_images.py
@@ -1,44 +1,57 @@
 from PIL import Image
 import os
+import shutil
 
-# Caminhos
+
 INPUT_DIR = "dataset"
 OUTPUT_DIR = "dataset_processed"
 
-IMAGE_SIZE = (400, 400)
+IMAGE_SIZE = (224, 224)
 JPEG_QUALITY = 90
 
 CLASSES = ["amora", "not_amora"]
 
-os.makedirs(OUTPUT_DIR, exist_ok=True)
-
 def process_image(input_path, output_path):
+    """Resize and save image"""
     try:
         with Image.open(input_path) as img:
             img = img.convert("RGB")
             img = img.resize(IMAGE_SIZE, Image.LANCZOS)
             img.save(output_path, "JPEG", quality=JPEG_QUALITY, optimize=True)
     except Exception as e:
-        print(f"Erro em {input_path}: {e}")
+        print(f"Error processing {input_path}: {e}")
 
-for cls in CLASSES:
-    input_class_dir = os.path.join(INPUT_DIR, cls)
-    output_class_dir = os.path.join(OUTPUT_DIR, cls)
 
-    os.makedirs(output_class_dir, exist_ok=True)
+def process_dataset(input_dir=INPUT_DIR, output_dir=OUTPUT_DIR):
+    """Process entire dataset"""
 
-    count = 1  # contador por classe
+    # Remove output folder if it exists
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
 
-    for filename in sorted(os.listdir(input_class_dir)):
-        if filename.lower().endswith((".jpg", ".jpeg", ".png")):
-            input_path = os.path.join(input_class_dir, filename)
+    os.makedirs(output_dir, exist_ok=True)
 
-            new_filename = f"{cls}_{count:04d}.jpg"
-            output_path = os.path.join(output_class_dir, new_filename)
+    for cls in CLASSES:
+        input_class_dir = os.path.join(input_dir, cls)
+        output_class_dir = os.path.join(output_dir, cls)
 
-            process_image(input_path, output_path)
-            count += 1
+        os.makedirs(output_class_dir, exist_ok=True)
 
-    print(f"✅ {cls}: {count - 1} imagens processadas")
+        count = 1
 
-print("concluido")
+        for filename in sorted(os.listdir(input_class_dir)):
+            if filename.lower().endswith((".jpg", ".jpeg", ".png")):
+                input_path = os.path.join(input_class_dir, filename)
+
+                new_filename = f"{cls}_{count:04d}.jpg"
+                output_path = os.path.join(output_class_dir, new_filename)
+
+                process_image(input_path, output_path)
+                count += 1
+
+        print(f"✅ {cls}: {count - 1} images processed")
+
+    print("Processing completed 🚀")
+
+if __name__ == "__main__":
+    process_dataset()
